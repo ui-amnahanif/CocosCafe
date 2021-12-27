ea91930b7178d7b9b6ed51400397d1be5ac75929
Izukucodes <izukcodes@gmail.com>
2021-12-27 15:05:45 +0500

    display message box

--- WindowsFormsApp1/ManagerHome.cs
+++ WindowsFormsApp1/ManagerHome.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void ManagerHome_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
